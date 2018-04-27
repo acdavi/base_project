7acdc3908c45904681486e9ad42467e6c1d9fdd9
LeonardoMelloSilvestri <leonardomscontato@gmail.com>
2018-04-26 22:14:28 -0300

    Autenticação

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180425231304) do
+ActiveRecord::Schema.define(version: 20180427004646) do
+
 
   create_table "aaas", force: :cascade do |t|
     t.string   "nome"
@@ -39,6 +40,28 @@ ActiveRecord::Schema.define(version: 20180425231304) do
     t.string   "nome"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "nome",                   default: "", null: false
+    t.string   "date_of_birth",          default: "", null: false
+    t.string   "email",                  default: "", null: false
+    t.string   "username",               default: "", null: false
+    t.string   "password",               default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.string   "phone_number"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
