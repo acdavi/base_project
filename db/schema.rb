16da88208de3cd7329f3e9e5eb4110f541a257e6
Breno Thales <breno.thales@gmail.com>
2018-05-02 17:12:15 -0300

    Adicionando as rotas

--- db/schema.rb
+++ db/schema.rb
@@ -55,5 +55,4 @@ ActiveRecord::Schema.define(version: 20180427005033) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
-
 end
