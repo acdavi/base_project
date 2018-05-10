52789e9d548d44dc19aa8ce7387f2999a1a7376a
acdavi <davimilt@gmail.com>
2018-05-09 22:07:13 -0300

    Ajustando rotas // Criando API

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
-  resources :cursos
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'pages#home'
+  	 devise_for :users
+  	 namespace :api do
+    	namespace :v1 do
+      	resources :contacts
+    	end
+  	end
 end
